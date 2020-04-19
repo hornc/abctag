4d9b94157fe5c4cec97a3dafdfd5b7ac0c417530
Charles Horn <charles.horn@gmail.com>
2020-04-19 15:54:24 +1200

    initial commit, web and CLI, with examples

---
+++ abct/abct.py
@@ -0,0 +1,6 @@
+from math import floor, log
+n   = lambda x: (x + 1) % 2 + 1
+s   = lambda x: floor(log(x + 1, 2))
+r   = lambda x: x // 2 - n(x) + 1 + n(x) * 2**(s(x) - 1)
+pn  = lambda p: r(p) + (n(p) == 2) * (r(r(p)) - r(p))
+dn  = lambda p, d: (d + (n(p) == n(d) == 2) * (n(r(p)) * 2**s(d)) - (n(p) == 1)) // (3 - n(p))
---
+++ abct/output.py
@@ -0,0 +1,8 @@
+from abct.abct import s
+
+# convert artihmetic bijective base-2 numeric value to BCT string of 0,1s
+bct = lambda i, s='': bct((i - 2 * ((i + 1) % 2)) // 2, '%s%d' % (s, (i + 1) % 2)) if i else s
+# Output encoded BCT bytes (with STX=\x02, ETX=\x03, start bit = 1, stop bit = 0 checking):
+bct_out = lambda s: b''.join([bytes([(int(s[len(s)%10:][i*10+1:i*10+9], 2)) for i in range(len(s)//10) if s[len(s)%10:][i*10] == '1' and s[len(s)%10:][(i+1)*10-1] == '0'])])[1:-1]
+abct_out = lambda i: bct_out(bct(i))
+has_out = lambda p, d: p // 2**(s(p) - 2) == 5 and d > 2**10 and d // 2**(s(d) - 10) == 2 * 3**6 - 7**2
