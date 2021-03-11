c02f6db484efbf6d1e0e9968a3c450da3f253543
Charles Horn <charles.horn@gmail.com>
2021-03-12 08:51:53 +1300

    fix typo and tidy comments

--- abct/output.py
+++ abct/output.py
@@ -1,8 +1,8 @@
 from abct.abct import s
 
-# convert artihmetic bijective base-2 numeric value to BCT string of 0,1s
+# Convert bijective base-2 numeric value to BCT string over {0, 1}
 bct = lambda i, s='': bct((i - 2 * ((i + 1) % 2)) // 2, '%s%d' % (s, (i + 1) % 2)) if i else s
-# convert BCT binary string into bijective base-2 int
+# Convert BCT binary string into bijective base-2 integer
 bin_to_bb2 = lambda s: sum([int(c) * 2 ** i for i,c in enumerate(s.replace(' ', '').replace('1', '2').replace('0', '1'))])
 # Output encoded BCT bytes (with STX=\x02, ETX=\x03, start bit = 1, stop bit = 0 checking):
 bct_out = lambda s: b''.join([bytes([(int(s[len(s)%10:][i*10+1:i*10+9], 2)) for i in range(len(s)//10) if s[len(s)%10:][i*10] == '1' and s[len(s)%10:][(i+1)*10-1] == '0'])])[1:-1]
