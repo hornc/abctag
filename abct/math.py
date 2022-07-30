8a4050ab058f088cb061c68c2fa61e16ecba873d
Charles Horn <charles.horn@gmail.com>
2022-07-30 15:51:51 +1200

    explain flog2()

--- abct/math.py
+++ abct/math.py
@@ -1,5 +1,10 @@
 def flog2(x):
-    """Floor of log2"""
+    """
+    Floor of log2.
+    Equivalent to math.floor(math.log(x, 2))
+    but does not give rounding errors on
+    x >= 2**48 - 1
+    """
     if x <= 0:
         raise ValueError("math domain error")
     return x.bit_length() - 1
